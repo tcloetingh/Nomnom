0f871db035ad1244db1daaa03fc93ba5e6c1b7f6
Tyler <tcloetingh@gmail.com>
2019-07-11 12:34:01 +0000

    lesson 27

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
   
   def create
-    current_user.places.create(place_params) 
-    redirect_to root_path
+    @place = current_user.places.create(place_params) 
+    if @place.valid? 
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -40,7 +44,7 @@ class PlacesController < ApplicationController
   def destroy
     @place = Place.find(params[:id])
     if @place.user != current_user
-      return render plain: 'Not Allowed' status: :forbidden
+      return render plain: 'Not Allowed', status: :forbidden
     end
 
     @place.destroy
