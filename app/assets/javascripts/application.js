216c6d66e014b58ea5f356c5b8355217613c6195
Tyler <tcloetingh@gmail.com>
2019-07-22 14:34:15 +0000

    lesson 39 halfway

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require activestorage
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+// require turbolinks
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,13 @@
+class PhotosController < ApplicationController
+  def create
+    @place = Place.find(params[:place_id])
+    @place.photos.create
+    redirect_to place_path(@place)
+  end
+
+private
+
+def photo_params
+  params.require(:photo).permit(:picture, :caption)
+  end
+end
