5c99ec9da44f832115ed5c0a97a191f6420e70b4
Tyler <tcloetingh@gmail.com>
2019-07-23 15:22:44 +0000

    L39 photos fix

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,7 @@ class Photo < ApplicationRecord
   mount_uploader :picture, PictureUploader
   belongs_to :user
   belongs_to :place
+
+  validates :picture, presence: true
+  validates :caption, presence: true
 end
